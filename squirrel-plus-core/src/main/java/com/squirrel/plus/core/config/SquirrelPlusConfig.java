ba90398b251fffd616a4c50de44395c12bae8403
liuccc <liuccc@jiedaibao.com>
2020-05-04 16:30:39 +0800

    config

---
+++ squirrel-plus-core/src/main/java/com/squirrel/plus/core/config/SquirrelPlusConfig.java
@@ -0,0 +1,19 @@
+package com.squirrel.plus.core.config;
+
+/**
+ * @author liucc
+ */
+public class SquirrelPlusConfig {
+
+    /**
+     * 配置中心地址
+     */
+    private String configCenterAddress;
+
+    /**
+     * 远程授权token
+     */
+    private String accessToken;
+
+
+}
